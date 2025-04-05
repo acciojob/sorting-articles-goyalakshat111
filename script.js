4c7cb027c9a5385d6ec8d482cad1b5587e49085f
Akshat Goyal <goyalakshat111@gmail.com>
2025-04-05 10:48:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const sortedBands = bands
+  .slice() // Create a copy of the array
+  .sort((a, b) =>
+    a.replace(/^(a |an |the )/i, '').localeCompare(
+      b.replace(/^(a |an |the )/i, '')
+    )
+  );
+
+
+
+
+let list = document.createElement("ul");
+list.id="band";
+
+for(let t of sortedBands){
+	let li = document.createElement("li");
+	li.innerText = t;
+	list.append(li)
+}
+
+
+document.body.append(list)
+
+
+
